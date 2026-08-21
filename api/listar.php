04cb3d75524f2fe9bc4756ed0f359b23c5377fa6
Matheus Dantas <matheusdantasamancio@gmail.com>
2026-08-20 21:31:01 -0300

    Començando a construir o buscar do front-end

--- api/listar.php
+++ api/listar.php
@@ -5,8 +5,15 @@ header("Content-Type: application/json; charset=UTF-8");
 
 include_once '../config/conexao.php';
 
-$consuta_produtos = "SELECT * FROM contatos";
+
+$json_front = file_get_contents("php://input");
+$dados_json = json_decode($json_front, true);
+
+$nome = "%".$dados_json["nome"]."%";
+
+$consuta_produtos = "SELECT * FROM contatos WHERE nome LIKE :nome";
 $dados_produtos = $conn->prepare($consuta_produtos);
+$dados_produtos->bindParam(':nome', $nome, PDO::PARAM_STR);
 $dados_produtos->execute();
 
 
@@ -16,7 +23,7 @@ if(($dados_produtos) && ($dados_produtos->rowCount() != 0))
       while($linha_produto = $dados_produtos->fetch(PDO::FETCH_ASSOC)){
         extract($linha_produto);
 
-        $lista_produtos[] = [
+        $return[] = [
             'id' => $id,
             'nome' => $nome,
             'telefone' => $telefone
@@ -24,6 +31,16 @@ if(($dados_produtos) && ($dados_produtos->rowCount() != 0))
       }  
 
       http_response_code(200);
-      echo json_encode($lista_produtos);
+      echo json_encode($return);
 
+    }else{
+      $return = [
+        "erro"=> false,
+        "mensagem" => "Nada encontrado"
+      ];
+
+      http_response_code(404);
+      echo json_encode($return);
     }
+
+    
