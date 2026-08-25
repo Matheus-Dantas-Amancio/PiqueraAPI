9b3c11b0ecdcd887c3bb25667cbf4b267ab6e5e0
Matheus Dantas <matheusdantasamancio@gmail.com>
2026-08-24 21:42:51 -0300

    Integração buscar contatos frontend

--- api/listar.php
+++ api/listar.php
@@ -1,5 +1,4 @@
 <?php
-
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 
@@ -7,40 +6,46 @@ include_once '../config/conexao.php';
 
 
 $json_front = file_get_contents("php://input");
-$dados_json = json_decode($json_front, true);
-
-$nome = "%".$dados_json["nome"]."%";
-
-$consuta_produtos = "SELECT * FROM contatos WHERE nome LIKE :nome";
-$dados_produtos = $conn->prepare($consuta_produtos);
-$dados_produtos->bindParam(':nome', $nome, PDO::PARAM_STR);
-$dados_produtos->execute();
+$nome_json = $_GET['nome'] ?? null;
+$nome_json = trim($nome_json);
 
 
+if ($nome_json != null) {
+  $nome = "%" . $nome_json . "%";
 
-if(($dados_produtos) && ($dados_produtos->rowCount() != 0))
-    {
-      while($linha_produto = $dados_produtos->fetch(PDO::FETCH_ASSOC)){
-        extract($linha_produto);
+  $consuta_produtos = "SELECT * FROM contatos WHERE nome LIKE :nome";
+  $dados_produtos = $conn->prepare($consuta_produtos);
+  $dados_produtos->bindParam(':nome', $nome, PDO::PARAM_STR);
+  $dados_produtos->execute();
 
-        $return[] = [
-            'id' => $id,
-            'nome' => $nome,
-            'telefone' => $telefone
-        ];
-      }  
+  if (($dados_produtos) && ($dados_produtos->rowCount() != 0)) {
+    while ($linha_produto = $dados_produtos->fetch(PDO::FETCH_ASSOC)) {
+      extract($linha_produto);
 
-      http_response_code(200);
-      echo json_encode($return);
-
-    }else{
-      $return = [
-        "erro"=> false,
-        "mensagem" => "Nada encontrado"
+      $return[] = [
+        'id' => $id,
+        'nome' => $nome,
+        'telefone' => $telefone
       ];
-
-      http_response_code(404);
-      echo json_encode($return);
     }
 
-    
+    http_response_code(200);
+    echo json_encode($return);
+  } else {
+    $return = [
+      "erro" => false,
+      "mensagem" => "Nada encontrado"
+    ];
+
+    http_response_code(404);
+    echo json_encode($return);
+  }
+} else {
+  $return = [
+    "erro" => false,
+    "mensagem" => "Digite alguma informação"
+  ];
+
+  http_response_code(404);
+  echo json_encode($return);
+}
--- public/script.js
+++ public/script.js
@@ -1,20 +1,19 @@
 
 
-const formulario =  document.querySelector("#salvar");
+const formulario = document.querySelector("#salvar");
 
-formulario.addEventListener("click", (e) =>{
-   
+formulario.addEventListener("click", (e) => {
+
 
     const nome = document.getElementById('nome').value;
     const telefone = document.getElementById('telefone').value;
-    console.log(nome,telefone);
 
-    const dados  = {
-            nome: nome,
-            telefone: telefone    
+    const dados = {
+        nome: nome,
+        telefone: telefone
     };
 
-        fetch('http://localhost/piqueraapi/api/inserir.php', {
+    fetch('http://localhost/piqueraapi/api/inserir.php', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
@@ -22,8 +21,50 @@ formulario.addEventListener("click", (e) =>{
         credentials: "include",
         body: JSON.stringify(dados),
     })
-    .then(data => console.log(data.json()));
-    
+        .then(data => console.log(data.json()));
+
 });
 
+
+const buscar = document.querySelector("#buscar");
+
+buscar.addEventListener("click", async (e) => {
+    const nomeLista = document.getElementById("buscarContatos").value;
+
+
+    const parametros = new URLSearchParams({
+        nome: nomeLista
+    });
+
+    await fetch(`http://localhost/piqueraapi/api/listar.php?${parametros}`, {
+        method: 'GET',
+        credentials: "include"
+    })
+        .then(dados => dados.json())
+        .then(data => {
+            console.log(data);
+            const tBody = document.querySelector("tbody");
+            tBody.innerHTML = "";
+
+            data.forEach(contato => {
+            const tr = document.createElement("tr");
+
+            tr.dataset.id = contato.id;
+            
+            tr.innerHTML += `
+                <td>${contato.id}</td>
+                <td>${contato.nome}</td>
+                <td>${contato.telefone}</td>
+                <td><button class="btn btn-outline-danger btn-sm"><i class="fa fa-trash" aria-hidden="true"></i></button> <i class="fa fa-pencil-square-o" aria-hidden="true"></i></td>
+            `;
+
+             tBody.appendChild(tr);
+            });
+           
+        });
+
+
+
+
+});
 
